201b13913ea7284bb94241b7ac41f5eb96cc1855
Emmie Smith <emmie.maeda@gmail.com>
2026-04-22 22:15:34 -0400

    Add first ideal spreads.

--- spread_classifier.py
+++ spread_classifier.py
@@ -3,9 +3,23 @@ def normalize_spread(stats):
     count = len(stats)
     return [(stat / total) * count for stat in stats]
 
+
 IDEAL_SPREADS = [
-    ("flat", )
+    ("flat", [1, 1, 1, 1, 1, 1]),
+    ("staircase", normalize_spread([10, 20, 30, 40, 50, 60])),
+    ("reverse staircase", normalize_spread([60, 50, 40, 30, 20, 10])),
+    # HP 50-80
+    # ATK 200
+    # DEF 50-80
+    # SPA 0
+    # SPD 50-80
+    # SPE 90-110
+    ("ideal atk", normalize_spread([80, 200, 80, 0, 80, 110])),
+    ("ideal spa", normalize_spread([80, 0, 80, 200, 80, 110])),
 ]
+
+# below 90 is "no speed"
+# cheez is one defense state is below 50
 
 
 def calculate_error(spread1, spread2):
@@ -15,6 +29,7 @@ def calculate_error(spread1, spread2):
 
 def calculate_confidence(total_error, count):
     return (2 * count - total_error) / (2 * count)
+
 
 def compare_spreads(spread):
     results = []
